58aa0830c1f6acbf95a09cc4c5c93d5d7a58d664
Angela Niederberger <angelaniederberger@protonmail.ch>
2021-10-23 20:18:32 +0200

    adding selection options

--- src/visualization/cineman_streamlit_app.py
+++ src/visualization/cineman_streamlit_app.py
@@ -3,59 +3,41 @@ import pandas as pd
 import plotly.graph_objects as go
 from datetime import date
 
+import plotting_functions as pf
+
 # Read in the data
 cineman_df = pd.read_csv(f"../../data/raw/{date.today()}_showtimes.csv", index_col=0)
 mapbox_access_token = open("../../.mapbox_token").read()
+movie_desc = pd.read_csv(f"../../data/raw/{date.today()}_zurich_movie_overviews.csv")
 
-
-# Plotting functions
-def create_plotly_map(df, access_token):
-    fig = go.Figure(go.Scattermapbox(
-        lat=df["latitude"],
-        lon=df["longitude"],
-        mode='markers',
-        marker=go.scattermapbox.Marker(
-            size=10, color="crimson"
-        ),
-        text=df["cinema"],
-        hoverinfo="text"
-    ))
-
-    fig.update_layout(
-        hovermode='closest',
-        width=500,
-        height=500,
-        margin={"r": 0, "t": 0, "l": 0, "b": 0},
-        mapbox=dict(
-            accesstoken=access_token,
-            center=go.layout.mapbox.Center(
-                lat=47.374,
-                lon=8.535
-            ),
-            zoom=12.5,
-            style="streets"
-        )
-    )
-    return fig
-
+# Cleaning (added to scraping script)
+cineman_df["dt_showtime"] = cineman_df["date"] + " " + cineman_df["showtime"]
+cineman_df["dt_showtime"] = pd.to_datetime(cineman_df["dt_showtime"], format= '%Y-%m-%d %H:%M')
 
 # Header and text
 st.title(f"Movies in Zurich, {date.today()}")
 
-# st.sidebar.header("Options for Selection")
+st.sidebar.header("Options for Selection")
 
 # Set up of the page
-st.sidebar.subheader("Options for Selection")
+#st.sidebar.subheader("Options for Selection")
 left, right = st.columns([2, 1])
 
-# Widgets
-# selectBox
+# Select the timeframe
+hours = ["All", "9-12am", "12-3pm", "3-6pm", "6-9pm", "9-12pm"]
+selected_hour = st.sidebar.select_slider("Show movies that start between:", options=hours)
+cineman_df_hour = pf.select_by_hour(cineman_df, hour=selected_hour)
 
-movies = ["All"]+sorted(pd.unique(cineman_df['movie']))
-movies = st.sidebar.selectbox("Choose a Movie", movies)   # Here the selection of the year.
+# Select a specific movie
+movies = ["All"]+sorted(pd.unique(cineman_df_hour['movie']))
+selected_movie = st.sidebar.selectbox("Choose a Movie", options=movies)   # Here the selection of the year.
+
+# Add the movie description
+overview = pf.fetch_movie_desc(movie_desc, selected_movie)
+st.sidebar.markdown(f'{overview}', unsafe_allow_html=True)
 
 # Create the map
-plotly_map = create_plotly_map(cineman_df, mapbox_access_token)
+plotly_map = pf.create_plotly_map(cineman_df_hour, mapbox_access_token, hour=selected_hour, movie=selected_movie)
 left.plotly_chart(plotly_map)
 
 # Credits for the data
---
+++ src/visualization/plotting_functions.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+
+def select_by_hour(df, hour="All"):
+    if hour == "All":
+        df = df
+    elif hour == "9-12am":
+        df = df[df["dt_showtime"].between('09:00:00', '12:00:00')]
+    elif hour == "12-3pm":
+        df = df[df["dt_showtime"].between('12:00:00', '15:00:00')]
+    elif hour == "3-6pm":
+        df = df[df["dt_showtime"].between('15:00:00', '18:00:00')]
+    elif hour == "6-9pm":
+        df = df[df["dt_showtime"].between('18:00:00', '21:00:00')]
+    else:
+        df = df[df["dt_showtime"].between('21:00:00', '23:59:00')]
+
+    return df
+
+
+def fetch_movie_desc(df, movie):
+    if movie in list(df["original_title"]):
+        overview = df[df["original_title"] == movie]["overview"].values[0]
+        if pd.isnull(overview):
+            overview = ""
+        else:
+            overview = f'<b>Movie Description:</b><br>{overview}'
+    else:
+        overview = ""
+    return overview
+
+
+# Plotly scattermapbox
+def create_plotly_map(df, access_token, hour="All", movie="All"):
+
+    if hour == "All":
+        df = df
+    elif hour == "9-12am":
+        df = df[df["dt_showtime"].between('09:00:00', '12:00:00')]
+    elif hour == "12-3pm":
+        df = df[df["dt_showtime"].between('12:00:00', '15:00:00')]
+    elif hour == "3-6pm":
+        df = df[df["dt_showtime"].between('15:00:00', '18:00:00')]
+    elif hour == "6-9pm":
+        df = df[df["dt_showtime"].between('18:00:00', '21:00:00')]
+    else:
+        df = df[df["dt_showtime"].between('21:00:00', '23:59:00')]
+
+    if movie == "All":
+        df = df
+    else:
+        df = df[df["movie"] == movie]
+
+    fig = go.Figure(go.Scattermapbox(
+        lat=df["latitude"],
+        lon=df["longitude"],
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=10, color="crimson"
+        ),
+        text=df["cinema"],
+        hoverinfo="text"
+    ))
+
+    fig.update_layout(
+        hovermode='closest',
+        width=500,
+        height=500,
+        margin={"r": 0, "t": 0, "l": 0, "b": 0},
+        mapbox=dict(
+            accesstoken=access_token,
+            center=go.layout.mapbox.Center(
+                lat=47.374,
+                lon=8.535
+            ),
+            zoom=12.5,
+            style="streets"
+        )
+    )
+    return fig
