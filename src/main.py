8dc31356965d0cb8112497a89b6ea4678dad4507
Angela Niederberger <angelaniederberger@protonmail.ch>
2022-01-15 22:50:14 +0100

    move modeling to scraping script

--- src/main.py
+++ src/main.py
@@ -1,71 +1,47 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import time
+#import time
 import pandas as pd
-import streamlit as st
-import s3fs
+from dotenv import find_dotenv, load_dotenv
+import os
+#import streamlit as st
+#import s3fs
 from datetime import date
-import pickle
+#import pickle
 
 from visualization.cineman_streamlit_app import create_app
-import features.corpus_prep as cp
-import models.reco_functions as rf
+#import features.corpus_prep as cp
+#import models.reco_functions as rf
 
 
 def main():
     # Create connection to AWS S3 Bucket
     # `anon=False` means not anonymous, i.e. it uses access keys to pull data.
-    fs = s3fs.S3FileSystem(anon=False)
+  #  fs = s3fs.S3FileSystem(anon=False)
 
-    # Reading in user-dependent variables: file paths
+    # Defining the file paths
     data_path_shows = f"s3://zmr-streamlit-aws/data/raw/{date.today()}_showtimes.csv"
     data_path_desc = f"s3://zmr-streamlit-aws/data/raw/{date.today()}_zurich_movie_overviews.csv"
-
-    # Try loading saved data on movies currently running in Zurich
-    cineman_df = pd.read_csv(data_path_shows)
-    movie_desc = pd.read_csv(data_path_desc)
-
-    # If the model is more than a week old, create the new corpus and retrain the model
     data_path_doc_sims = "s3://zmr-streamlit-aws/models/document_similarities.csv"
     data_path_comb_corpus = "s3://zmr-streamlit-aws/data/processed/comb_movie_corpus.csv"
 
-    if time.time() - fs.modified(data_path_doc_sims).timestamp() > 600000:
-        # Try loading saved processed data on old movies
-        data_path_5000_proc = "s3://zmr-streamlit-aws/data/processed/tmdb_5000_movies_proc.csv"
-        try:
-            movie_desc_5000 = pd.read_csv(data_path_5000_proc)
-        # If this processed file doesn't exist yet, create it from the external data file from kaggle
-        except FileNotFoundError:
-            data_path_5000_ext = "s3://zmr-streamlit-aws/data/external/tmdb_5000_movies.csv"
-            movies_kaggle = pd.read_csv(data_path_5000_ext)
-            movie_desc_5000 = cp.process_kaggle_tmdb_dataset(kaggle_file=movies_kaggle)
-            movie_desc_5000.to_csv(data_path_5000_proc)
+    # Load saved data on movies currently running in Zurich
+    cineman_df = pd.read_csv(data_path_shows)
+    movie_desc = pd.read_csv(data_path_desc)
 
-        # Create the corpus
-        all_movies_corpus, norm_movie_desc = cp.generate_movie_corpus(old_movies_desc=movie_desc_5000,
-                                                                   recent_movies_desc=movie_desc)
-        all_movies_corpus.to_csv(data_path_comb_corpus)
-
-        # Tokenize the corpus and train the model
-        ft_model_path = "s3://zmr-streamlit-aws/models/fast_text_model.sav"
-        tokenized_docs, ft_model = rf.train_ft_model(norm_movie_desc)
-        pickle.dump(ft_model, fs.open(ft_model_path, 'wb'))
-
-        # Calculate document vectors and similarities
-        doc_similarities = rf.calc_cosine_similarity(corpus=tokenized_docs, model=ft_model, num_features=300)
-        doc_similarities.to_csv(data_path_doc_sims)
-
-    else:
-        all_movies_corpus = pd.read_csv(data_path_comb_corpus, index_col=0)
-        doc_similarities = pd.read_csv(data_path_doc_sims, index_col=0)
+    # Load the full movies corpus and document similarity matrix
+    all_movies_corpus = pd.read_csv(data_path_comb_corpus, index_col=0)
+    doc_similarities = pd.read_csv(data_path_doc_sims, index_col=0)
 
     # Create the app
-    mapbox_access_token = st.secrets["MAPBOX_ACCESS_TOKEN"]
+ #   mapbox_access_token = st.secrets["MAPBOX_ACCESS_TOKEN"]
+    mapbox_access_token = os.getenv("MAPBOX_ACCESS_TOKEN")
 
     create_app(cineman_df=cineman_df, movie_desc=movie_desc, all_movies_desc=all_movies_corpus,
                similarities_df=doc_similarities, MAPBOX_ACCESS_TOKEN=mapbox_access_token)
 
 
 if __name__ == '__main__':
+    load_dotenv(find_dotenv())
     main()
---
+++ src/scraper.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import s3fs
+from dotenv import find_dotenv, load_dotenv
+import os
+from datetime import date
+import logging
+import time
+import pandas as pd
+import pickle
+
+import data.cineman_scraping as cs
+from data.tmdb_api import get_specific_movie_overviews
+import features.corpus_prep as cp
+import models.reco_functions as rf
+
+
+def scraper():
+    # Start logging
+    logging.basicConfig(level=logging.INFO)
+
+    # Defining file paths
+    data_path_shows = f"s3://zmr-streamlit-aws/data/raw/{date.today()}_showtimes.csv"
+    data_path_desc = f"s3://zmr-streamlit-aws/data/raw/{date.today()}_zurich_movie_overviews.csv"
+    tmdb_ids_file_path = "s3://zmr-streamlit-aws/data/external/tmdb_id_file.gz"
+
+    # Getting credentials
+    tmdb_credentials_path = os.getenv("TMDB_CREDENTIALS_PATH")
+    logging.info('TMDB credentials fetched')
+
+    # Create connection to AWS S3 Bucket
+    fs = s3fs.S3FileSystem(anon=False)
+
+    # Scraping the most recent data and saving it
+    content = cs.scrape_cineman(cities=("Zürich"))
+    movie_program_df = cs.format_cineman_content(html_content=content)
+    cineman_df = cs.add_theatre_coordinates(showtimes_df=movie_program_df)
+    cineman_df.to_csv(data_path_shows)
+    logging.info('First data file saved to bucket')
+
+    # Requesting movie overviews via the tmdb api and saving them
+    movie_desc = get_specific_movie_overviews(TMDB_IDS_FILE_PATH=tmdb_ids_file_path,
+                                              TMDB_CREDENTIALS_PATH=tmdb_credentials_path,
+                                              DATA_PATH_SHOWS=data_path_shows,
+                                              movies_list=None)
+    movie_desc.to_csv(data_path_desc)
+    logging.info('Second data file saved to bucket')
+
+    # If the model is more than a week old, create the new corpus and retrain the model
+    data_path_doc_sims = "s3://zmr-streamlit-aws/models/document_similarities.csv"
+    data_path_comb_corpus = "s3://zmr-streamlit-aws/data/processed/comb_movie_corpus.csv"
+
+    if time.time() - fs.modified(data_path_doc_sims).timestamp() > 600000:
+        logging.info('Retraining model')
+        # Try loading saved processed data on old movies
+        data_path_5000_proc = "s3://zmr-streamlit-aws/data/processed/tmdb_5000_movies_proc.csv"
+        try:
+            movie_desc_5000 = pd.read_csv(data_path_5000_proc)
+        # If this processed file doesn't exist yet, create it from the external data file from kaggle
+        except FileNotFoundError:
+            data_path_5000_ext = "s3://zmr-streamlit-aws/data/external/tmdb_5000_movies.csv"
+            movies_kaggle = pd.read_csv(data_path_5000_ext)
+            movie_desc_5000 = cp.process_kaggle_tmdb_dataset(kaggle_file=movies_kaggle)
+            movie_desc_5000.to_csv(data_path_5000_proc)
+
+        # Create the corpus
+        all_movies_corpus, norm_movie_desc = cp.generate_movie_corpus(old_movies_desc=movie_desc_5000,
+                                                                   recent_movies_desc=movie_desc)
+        all_movies_corpus.to_csv(data_path_comb_corpus)
+
+        # Tokenize the corpus and train the model
+        ft_model_path = "s3://zmr-streamlit-aws/models/fast_text_model.sav"
+        tokenized_docs, ft_model = rf.train_ft_model(norm_movie_desc)
+        pickle.dump(ft_model, fs.open(ft_model_path, 'wb'))
+        logging.info('Retrained model saved to bucket')
+
+        # Calculate document vectors and similarities
+        doc_similarities = rf.calc_cosine_similarity(corpus=tokenized_docs, model=ft_model, num_features=300)
+        doc_similarities.to_csv(data_path_doc_sims)
+        logging.info('Document similarity matrix saved to bucket')
+
+
+if __name__ == '__main__':
+    load_dotenv(find_dotenv())
+    scraper()
